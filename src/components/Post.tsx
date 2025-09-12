29b205dd7c2a8a618fe2746be5852f16a4be17f3
Altaïr <notstaylost@gmail.com>
2025-09-12 19:18:42 +0200

    fix authorId display

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,11 +5,12 @@ import { type IPost } from './PostsLists';
 const Post: React.FC<IPost> = async ({ authorId, content, createdAt, id }) => {
   const session = await getSession();
   const isAuthor: boolean = authorId === session.sessionId;
+
   return (
     <div className="flex px-2 py-1.5 rounded border">
       <div className="flex flex-col w-full">
         <p className="text-xs font-mono">
-          by {authorId.split('').splice(0, 6)}&hellip; on {createdAt ? createdAt.toLocaleString() : 'unknown'}
+          by {authorId.split('').splice(0, 6).join('')}&hellip; on {createdAt ? createdAt.toLocaleString() : 'unknown'}
         </p>
 
         <div className="flex justify-between">
